497f944b9f29399dd6d6e1383e42c21d4475d367
Jaime Salmeron <expresscliente138@gmail.com>
2026-02-03 10:43:35 -0600

    Desarrollamos la interfaz grafica de una calculadora y se realizo la funcionalidad de la calculadora basica.

--- app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.miprimeraapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tempVal;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn = findViewById(R.id.btnCalcular);
+        btn.setOnClickListener(v ->calcular());
+    }
+    private void calcular(){
+        tempVal = findViewById(R.id.txtNum1);
+        double num1 = Double.parseDouble(tempVal.getText().toString());
+
+        tempVal = findViewById(R.id.txtNum2);
+        double num2 = Double.parseDouble(tempVal.getText().toString());
+
+        double respuesta = num1 + num2;
+
+        tempVal = findViewById(R.id.lblRespuesta);
+        tempVal.setText("Respuesta: "+ respuesta);
 
     }
 }
