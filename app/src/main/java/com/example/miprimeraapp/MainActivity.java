bbb1d1c58631781b5585b7da9cd1c68f6ad9b293
Jaime Salmeron <expresscliente138@gmail.com>
2026-02-05 19:39:55 -0600

    Le agregamos la opciones de suma, resta, multiplicacion, division, factorial, porcentaje, exponenciación, raiz.

--- app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.miprimeraapp;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tempVal;
     Button btn;
+    RadioButton opt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,10 +31,49 @@ public class MainActivity extends AppCompatActivity {
         tempVal = findViewById(R.id.txtNum2);
         double num2 = Double.parseDouble(tempVal.getText().toString());
 
-        double respuesta = num1 + num2;
+        double respuesta = 0;
+
+        opt = findViewById(R.id.optSuma);
+        if(opt.isChecked()) {
+            respuesta = num1 + num2;
+        }
+        opt = findViewById(R.id.optResta);
+        if(opt.isChecked()) {
+            respuesta = num1 - num2;
+        }
+        opt = findViewById(R.id.optMultiplicar);
+        if(opt.isChecked()) {
+            respuesta = num1 * num2;
+        }
+        opt = findViewById(R.id.optDivision);
+        if(opt.isChecked()) {
+            respuesta = num1 / num2;
+        }
+        opt = findViewById(R.id.optFactorial);
+        if(opt.isChecked()) {
+            int n = (int) num1;
+            double acumulador = 1;
+            for (int i = 1; i <= n; i++) {
+                acumulador = acumulador * i;
+                respuesta = acumulador;
+            }
+        }
+        opt = findViewById(R.id.optPorcentaje);
+        if(opt.isChecked()) {
+            respuesta = (num1 * num2) /100;
+        }
+        opt = findViewById(R.id.optExponenciacion);
+        if(opt.isChecked()) {
+            double base = num1;
+            double exponente = num2;
+            respuesta = Math.pow(base, exponente);
+        }
+        opt = findViewById(R.id.optRaiz);
+        if(opt.isChecked()) {
+            respuesta = Math.sqrt(num1);
+        }
 
         tempVal = findViewById(R.id.lblRespuesta);
         tempVal.setText("Respuesta: "+ respuesta);
-
     }
 }
