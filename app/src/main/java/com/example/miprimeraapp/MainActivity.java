f8d378dd2379cd6ea9bcd04754094f4d8ec8b56a
bairesrodrigo <bairesrodrigo82@gmail.com>
2026-02-28 19:45:33 -0600

    Usando el RadioGroup y el ID para saber que operacion se realizara

--- app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.miprimeraapp;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tempVal;
     Button btn;
+    RadioGroup radioGroup;
     RadioButton opt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,56 +24,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn = findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(v ->calcular());
+        btn.setOnClickListener(v->calcular());
     }
     private void calcular(){
         tempVal = findViewById(R.id.txtNum1);
-        double num1 = Double.parseDouble(tempVal.getText().toString());
+        Double num1 = Double.parseDouble(tempVal.getText().toString());
 
         tempVal = findViewById(R.id.txtNum2);
-        double num2 = Double.parseDouble(tempVal.getText().toString());
+        Double num2 = Double.parseDouble(tempVal.getText().toString());
 
         double respuesta = 0;
 
-        opt = findViewById(R.id.optSuma);
-        if(opt.isChecked()) {
+        radioGroup = findViewById(R.id.optOpciones);
+        if(radioGroup.getCheckedRadioButtonId()==R.id.optSuma) {
             respuesta = num1 + num2;
         }
-        opt = findViewById(R.id.optResta);
-        if(opt.isChecked()) {
+        if(radioGroup.getCheckedRadioButtonId()==R.id.optResta) {
             respuesta = num1 - num2;
         }
-        opt = findViewById(R.id.optMultiplicar);
-        if(opt.isChecked()) {
+        if(radioGroup.getCheckedRadioButtonId()==R.id.optMultiplicar) {
             respuesta = num1 * num2;
         }
-        opt = findViewById(R.id.optDivision);
-        if(opt.isChecked()) {
+        if(radioGroup.getCheckedRadioButtonId()==R.id.optDividir) {
             respuesta = num1 / num2;
         }
-        opt = findViewById(R.id.optFactorial);
-        if(opt.isChecked()) {
-            int n = (int) num1;
-            double acumulador = 1;
-            for (int i = 1; i <= n; i++) {
-                acumulador = acumulador * i;
-                respuesta = acumulador;
-            }
-        }
-        opt = findViewById(R.id.optPorcentaje);
-        if(opt.isChecked()) {
-            respuesta = (num1 * num2) /100;
-        }
-        opt = findViewById(R.id.optExponenciacion);
-        if(opt.isChecked()) {
-            double base = num1;
-            double exponente = num2;
-            respuesta = Math.pow(base, exponente);
-        }
-        opt = findViewById(R.id.optRaiz);
-        if(opt.isChecked()) {
-            respuesta = Math.sqrt(num1);
-        }
+
 
         tempVal = findViewById(R.id.lblRespuesta);
         tempVal.setText("Respuesta: "+ respuesta);
