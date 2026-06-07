f760a2cdcebe8344d3ab592dc319c396387edd96
bairesrodrigo <bairesrodrigo82@gmail.com>
2026-06-07 09:34:16 -0600

    Permisos en android para la app de mensajeria

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,18 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation("com.google.firebase:firebase-database:22.0.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // 1. El Firebase BoM (maneja las versiones automáticamente)
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+
+    // 2. Las librerías de Firebase (ESTAS NO LLEVAN PLATFORM)
+    implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-messaging")
+    implementation("com.google.firebase:firebase-storage")
+
+    // 3. Firebase UI para Storage
+    implementation("com.firebaseui:firebase-ui-storage:8.0.0")
 }
